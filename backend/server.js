a68b6713878750e7b0f63c6ba440fb84d23d7bb1
Apoorva Chaudhary <chaudharyapoorva0187@gmail.com>
2025-12-12 23:55:26 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -32,3 +32,4 @@ app.use("/", require("./routes/sosRoutes"));
 // ---- LISTEN ----
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
